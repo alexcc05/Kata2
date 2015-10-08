7f98001acca67cbdf9d1062a86a665804f1a27cc
usuario <usuario@10.22.143.36>
2015-10-08 10:14:39 +0100

    kata2 version 1

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -10,14 +10,14 @@ import java.util.HashMap;
  *
  * @author usuario
  */
-public class Kata2 {
+public class Kata2  {
 
     public static void main(String[] args) {
        int[] array= {1,4,7,1,1,8,4,1,4,9};
        Histogram histo = new Histogram(array);
        HashMap<Integer, Integer> histogram = histo.getHisto();
    
-       for(Integer key:histogram.keySet()){
+       for(Object key:histogram.keySet()){
           System.out.println(key +":"+ histogram.get(key));
     }
     
