29ef6298d2aca85cb2a357449e854f67739316a6
usuario <usuario@10.22.143.36>
2015-10-08 10:14:56 +0100

    kata2 version 2

--- src/kata2/Histogram.java
+++ src/kata2/Histogram.java
@@ -3,18 +3,18 @@
  * and open the template in the editor.
  */
 package kata2;
-
 import java.util.HashMap;
+public class Histogram <T>{
+    private final T[] vector;
 
-public class Histogram {
-    private final int[] vector;
-
-    public Histogram(int[] vector) {
+    public Histogram(T[] vector) {
         this.vector = vector;
     }
+
     
-    public HashMap<Integer,Integer> getHisto(){
-        HashMap<Integer,Integer> histo= new HashMap<>();
+    
+    public HashMap<T,Integer> getHisto(){
+        HashMap<T,Integer> histo= new HashMap<>();
          for(int i=0;i<vector.length;i++){
            if(!histo.containsKey(vector[i])){
                histo.put(vector[i],1);
@@ -24,6 +24,5 @@ public class Histogram {
        }
       return histo;
     }
-    
-    
+  
 }
